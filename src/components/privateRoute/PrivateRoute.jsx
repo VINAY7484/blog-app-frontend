9e9ca6fd638b0709fbf53934858568461555644b
VINAY7484 <70506939+VINAY7484@users.noreply.github.com>
2024-08-09 13:17:37 +0530

    first commi

---
+++ src/components/privateRoute/PrivateRoute.jsx
@@ -0,0 +1,25 @@
+import { Navigate, Outlet } from 'react-router-dom';
+//  const PrivateRoute = ({ isAuthenticated, ...props }) => {
+    
+
+  
+  export default function PrivateRoute({ isAuthenticated, ...props }) {
+    const token = sessionStorage.getItem('accessToken');
+    
+    return isAuthenticated && token ? 
+      <>
+        {/* <Header /> */}
+        <Outlet />
+      </> : <Navigate replace to='/account' />
+  };
+  
+  
+  
+//   const PrivateRoute = () => {
+//     return (
+      
+//     )
+//   }
+  
+//   export default PrivateRoute
+  
